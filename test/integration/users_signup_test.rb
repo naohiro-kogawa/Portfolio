5a20bbb1efff8acb223f3603ce786c14c21030fb
Naohiro <sakuraxsakuraxis@gmail.com>
2018-10-25 14:57:15 +0000

    7.4sai

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,19 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+　 test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'   
+  end  
+　
 　test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -12,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert_not flash.empty? 
   end
 end
