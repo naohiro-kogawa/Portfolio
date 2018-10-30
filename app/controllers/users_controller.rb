b1a53cd267239094ca42752625cc192ef6af6541
Naohiro <sakuraxsakuraxis@gmail.com>
2018-10-30 07:17:39 +0000

    10.1 complite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,20 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
-  end  
+  end 
   
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user      
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+   
     private
 
     def user_params
