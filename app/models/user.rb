5291ef43fee61dd0d552178070848430a441423d
Naohiro <sakuraxsakuraxis@gmail.com>
2018-11-06 14:40:43 +0000

    13.3.3　complite

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,13 @@ class User < ApplicationRecord
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
-  end  
+  end 
+  
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   
   private
 
