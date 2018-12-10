9a3fb1fc7978c2cb7e3f68538fb828d00deffc45
EC2 Default User <ec2-user@ip-172-31-14-229.us-east-2.compute.internal>
2018-12-10 13:01:52 +0000

    nihonngoka2.1-complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
+      # if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
-      else
-        message  = "アカウントが有効になっていません。"
-        message += "メールを確認してアカウントを有効にして下さい。"
-        flash[:warning] = message
-        redirect_to root_url
-      end
+      # else
+      #   message  = "アカウントが有効になっていません。"
+      #   message += "メールを確認してアカウントを有効にして下さい。"
+      #   flash[:warning] = message
+      #   redirect_to root_url
+      # end
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが無効です'      
       # エラーメッセージを作成する
